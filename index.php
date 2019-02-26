ec95926ee623f204be1528e18f85f67515b3f2a3
Watheq <watheq.alshowaiter@gmail.com>
2019-02-26 17:35:37 +0300

    adding CDN's for those who prefer it

--- index.php
+++ index.php
@@ -5,51 +5,114 @@
     <meta name="viewport" content="width=device-width">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <!-- linking bootstrap  -->
-     <link rel="stylesheet" type="text/css" href="vendor/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="vendor/dataTables.bootstrap4.css">
-<!-- CDN -->
-    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+<!-- 
+    THIS linking is for make it easy 
+    the most es=ffecient way is to 
+    make sctipts below <body> 
 
+    data tabels plugin requires 
+
+    bootsrap.css
+    bootstrap.js 
+    jquery.js 
+
+    and the plugin itself contains 
+    jquery.datatable.js 
+    datatable.bootstrap.css 
+    datatables.bootsrap.js
+
+    these libraries I've used are downloaded locally 
+    you can use CDN's 
+ -->
+
+    <!-- linking  jquery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+
+     <!-- linking bootstrap  CSS and JS -->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- linking data tables bootstrap (CSS+JS) and jquery JS -->
+    <link rel="stylesheet" type="text/css" href="vendor/datatables/dataTables.bootstrap4.min.css">
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script> 
+    
+    
+<!-- CDN -->
+     <!-- linking  jquery -->
+     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+    
+    <!-- linking bootstrap  CSS and JS -->
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+
+    <!-- linking data tables bootstrap (CSS+JS) and jquery JS -->
+    <!-- <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"> -->
+    <!-- <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script> -->
+
+<!-- End CDN -->
+
+<?php 
+    $conn = mysqli_connect("localhost", "root", "", "classicmodels") ;
+    $query = "select * from employees"; 
+    $result = mysqli_query($conn, $query); 
+?>
 
     
     <title>Use Datatable</title>
 </head>
 <body>
-    <h2>Data tables</h2>
+    
 
     <div class="container">
-
-    <div class="">
+    <br>
+    <h2 class="text-center alert alert-info">Using Data tables with PHP and MySQL</h2>
+    <!-- <div class=""> -->
     <table id="table_id" class="display table table-bordered">
     <thead>
         <tr>
-            <th>User</th>
-            <th>Posts</th>
+            <th>Employee number</th>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>Extention</th>
+            <th>email</th>
+            <th>Office Code</th>
+            <th>reports to </th>
+            <th>Job title</th>
+
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>Ali</td>
-            <td>Post 1</td>
-        </tr>
-        <tr>
-            <td>Ahmed</td>
-            <td>Post 2</td>
-        </tr>
+       <?php while($row = mysqli_fetch_array($result))
+       {
+           echo ' 
+                <tr>    
+                    <td>'.$row["employeeNumber"].'</td> 
+                    <td>'.$row["lastName"].'</td> 
+                    <td>'.$row["firstName"].'</td> 
+                    <td>'.$row["extension"].'</td> 
+                    <td>'.$row["email"].'</td> 
+                    <td>'.$row["officeCode"].'</td> 
+                    <td>'.$row["reportsTo"].'</td> 
+                    <td>'.$row["jobTitle"].'</td> 
+
+                </tr> 
+           ';
+       }
+       ?> 
     </tbody>
 </table>
     </div>
     
-    
+    <footer class="footer">
+       <br>
+    </footer>
 </div>
+
     
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <!-- <script src="vendor/jquery.dataTables.min.js"></script> -->
-<!-- CDN  -->
-    <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"> </script>
     <script>
-        $(document).ready( function () {
+    $(document).ready( function () {
     $('#table_id').DataTable(
     ); 
 } );
